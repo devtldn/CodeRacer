210a87522c28920357edb6b3b1f1c2462174f906
devtldn <devtldn@gmail.com>
2018-08-10 14:11:41 -0700

    Tentative starter files and structure created

---
+++ models/racer.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const codeRacerSchema = new Schema({
+
+});
+
+const Racer = mongoose.model("Racer", nytreactSchema);
+
+module.exports = Racer;
