f9fafabbd29c077014c7ec0671676c11e2c81f55
Albert Kim <miktrebla92@gmail.com>
2018-08-13 16:50:19 -0700

    added button to test start dummy data and show hidden input box

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,1 +1,19 @@
 
+document.addEventListener("DOMContentLoaded", function(event){
+    var button = document.getElementById("button");
+    var container = document.getElementById("main-container");
+    var input = document.getElementById('user-input');
+
+    button.onclick = function(event) {
+        event.preventDefault();
+        console.log('clickity click')
+        axios.get('/api/racer').then(res => {
+            container.innerHTML+=`<p>${res.data[0].text}</p>`
+            input.setAttribute("style", "display:block;")
+        })
+    }
+
+  
+  
+
+});
